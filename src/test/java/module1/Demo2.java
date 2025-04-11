090878a0ceb09a8e4dcc34856b81e3ad49289e01
Ganesh <vmwms99@gmail.com>
2025-04-12 01:22:54 +0530

    added Demo2 file

---
+++ src/test/java/module1/Demo2.java
@@ -0,0 +1,17 @@
+package module1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 10,b = 10;
+		
+		int c;
+		 c= a+b;
+		 
+		 System.out.println("adddtion"+c);
+
+	}
+
+}
--- src/test/java/module1/Demotest1.java
+++ src/test/java/module1/Demotest1.java
@@ -4,6 +4,7 @@ public class Demotest1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello there");
+		System.err.println("hyeeeeee");
 
 	}
 
